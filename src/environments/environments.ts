17be4fcb8da5e454a13f76098c44e247b4506482
DavidReaIFTS24 <40808658@ifts24.edu.ar>
2025-10-25 15:45:06 -0300

    Primer commit: Estructura inicial del proyecto

---
+++ src/environments/environments.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "tu-api-key",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    projectId: "tu-proyecto-id",
+    storageBucket: "tu-proyecto.appspot.com",
+    messagingSenderId: "tu-sender-id",
+    appId: "tu-app-id"
+  },
+  apiUrl: "http://localhost:3000/api" 
+};
